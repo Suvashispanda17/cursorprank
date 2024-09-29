2a480c0c0e71bc7b09d2a2b5ad296d6d8338f281
Suvashispanda17 <pandasuvashis17@gmail.com>
2024-09-29 08:20:44 +0530

    cur.py

--- cur.py
+++ cur.py
@@ -1,24 +1,3 @@
-# import pyautogui as pag
-# import random
-# import time
-# import keyboard as ke
-# import cowsay as cs
-
-# while True:
-#     if ke.is_pressed('q'):
-#         cs.daemon("you got pranked my friend.......")
-#         print("you got pranked my friend.......")
-#         break
-#     # Move the mouse to a random location on the screen
-#     x= random.randint(500,1000)
-#     y= random.randint(200,600)
-#     pag.moveTo(x,y)
-#     # Move the mouse to a random location on the screen
-#     time.sleep(3) # wait for 3 seconds before moving again
-    
-
-
-
 import pyautogui as pag
 import random
 import time
@@ -31,7 +10,7 @@ while True:
     y = random.randint(200, 600)
     pag.moveTo(x, y)
     
-    # Wait for short intervals (split 3 seconds into smaller intervals to allow key detection)
+    # wait for short intervals (split 3 seconds into smaller intervals to allow key detection)
     for i in range(30):  # This loops for 3 seconds (30 * 0.1 = 3)
         if ke.is_pressed('q'):
             cs.daemon("you got pranked my friend.......")
